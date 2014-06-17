5ee94d2b29a8eabbeb9f97da09389eddd267218b
SanAndreasP <1294@live.de>
2014-06-17 15:30:16 +0200

    tried images as a button, terribly failed... I'll just do a C++ overlay for that program, ugh...

--- fGradleH_gui.py
+++ fGradleH_gui.py
@@ -1,5 +1,5 @@
 import ttk
-
+import Tkinter
 
 __author__ = 'SanAndreas'
 
@@ -7,12 +7,14 @@ __author__ = 'SanAndreas'
 class Application(ttk.Frame):
     def __init__(self, master=None):
         ttk.Frame.__init__(self, master)
-        self.pack()
-        #self.grid()
-        self.quitButton = ttk.Button(self, text='Quit', command=self.quit, width=100)
+        self.quitButton = Tkinter.Button(self, text='Quit', command=self.quit, width=100)
+        self.btnImg = Tkinter.PhotoImage(file="test_button.png")
+        self.quitButton.config(image=self.btnImg, width="389", height="34")
         self.quitButton.pack()
 
-app = Application()
+root = Tkinter.Tk()
+app = Application(root)
 app.master.title('Sample application')
+root.geometry('640x480')
 app.mainloop()
 
